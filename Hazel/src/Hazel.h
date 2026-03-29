650c8488afdd651c4f8ca54dc497d767e962260c
lithuanianlegend-dev <lithuanianlegend@gmail.com>
2026-03-29 20:41:20 +0300

    Event system!

--- Hazel/src/Hazel.h
+++ Hazel/src/Hazel.h
@@ -1,7 +1,9 @@
 #pragma once
 
 // For use by Hazel applications.
+
 #include "Hazel/Application.h"
+#include "Hazel/Log.h"
 
 // -- ENTRY POINT --------------------------
 #include "Hazel/EntryPoint.h"
